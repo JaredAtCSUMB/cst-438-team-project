9bd5187f76940093c9a407251a9f6e9cf04e2fb1
Andrew <ameraz@csumb.edu>
2020-04-21 02:42:02 -0700

    Updated City Repository, City, and index.html page to allow for index AJAX POST request, also updated the jquery to not use slim version

--- cst438/src/main/java/cst438/controller/CityRestController.java
+++ cst438/src/main/java/cst438/controller/CityRestController.java
@@ -6,16 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cst438.domain.*;
 import cst438.service.CityService;
+
 
 @RestController
 public class CityRestController {
 	
 	@Autowired
 	private CityService cityService;
+	
+	@Autowired
+	private CityRepository cityRepository;
 	
 	@GetMapping("/api/cities")
 	public ResponseEntity<?> getCities() {
@@ -27,4 +34,16 @@ public class CityRestController {
 
 		return new ResponseEntity<List<City>>(cities, HttpStatus.OK);
 	} 
+	
+	@PostMapping("/api/cityFromCountry")
+	public ResponseEntity<?> getCountry(@RequestParam("countryName") String countryName) {
+		List<City> cities = cityRepository.findAllByOrderByNameAsc(countryName);
+		
+		if(cities == null)
+		{
+			return new ResponseEntity<City>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<List<City>>(cities, HttpStatus.OK);
+	}
 }
